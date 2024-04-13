0c36dcaf595551c52a8c3d53ed44910e2f82267a
Alfred Schilken <alfred@schilken.de>
2024-04-13 17:06:02 +0200

    add fastForward and fastRewind by 60 sec

--- lib/src/utils/extensions/video_controller_extensions.dart
+++ lib/src/utils/extensions/video_controller_extensions.dart
@@ -3,9 +3,15 @@ import 'package:video_player/video_player.dart';
 /// Extensions for the [VideoPlayerController]'s object.
 extension VideoControllerExtension on VideoPlayerController {
   /// Extension to forward the video's current position.
-  Future<void> fastForward() async {
+  Future<void> forward() async {
     if (value.duration.inSeconds - value.position.inSeconds > 10) {
       return seekTo(Duration(seconds: value.position.inSeconds + 10));
+    }
+  }
+
+  Future<void> fastForward() async {
+    if (value.duration.inSeconds - value.position.inSeconds > 60) {
+      return seekTo(Duration(seconds: value.position.inSeconds + 60));
     }
   }
 
@@ -13,6 +19,14 @@ extension VideoControllerExtension on VideoPlayerController {
   Future<void> rewind() async {
     if (value.position.inSeconds > 10) {
       return seekTo(Duration(seconds: value.position.inSeconds - 10));
+    } else {
+      return seekTo(const Duration(seconds: 0));
+    }
+  }
+
+  Future<void> fastRewind() async {
+    if (value.position.inSeconds > 50) {
+      return seekTo(Duration(seconds: value.position.inSeconds - 50));
     } else {
       return seekTo(const Duration(seconds: 0));
     }
